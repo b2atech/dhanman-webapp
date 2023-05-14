646dc0f6e35a772fd6cab2ddca37e397a8fde8ff
Bharat <bharat.mane@gmail.com>
2023-05-14 17:32:44 +0530

    fix: restructure the folders

--- src/package/firebase-jwt/src/_apis_/mock.js
+++
@@ -1,8 +0,0 @@
-import AxiosMockAdapter from 'axios-mock-adapter';
-import axios from '../components/jwt/axios';
-
-const axiosMockAdapter = new AxiosMockAdapter(axios, {
-  delayResponse: 0,
-});
-
-export default axiosMockAdapter;
--- src/package/firebase-jwt/src/components/authGurad/useAuth.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import AuthContext from '../firebase/FirebaseContext';
-//import { AuthContext } from '../jwt/JwtContext';
-
-const useAuth = () => useContext(AuthContext);
-
-export default useAuth;
