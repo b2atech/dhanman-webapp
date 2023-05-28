7fc2c86b78d3a74d6a32e766a972e1fefca177e4
bharat.mane <bharat.mane@gmail.com>
2023-05-28 15:08:32 +0530

    fix: path errors

--- src/layouts/logo/AuthLogo.js
+++ src/layouts/logo/AuthLogo.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import { ReactComponent as LogoDarkIcon } from '../../assets/images/logos/dark-logo-icon.svg';
-import { ReactComponent as LogoDarkText } from '../../assets/images/logos/dark-logo-text.svg';
-import { ReactComponent as LogoWhiteIcon } from '../../assets/images/logos/white-logo-icon.svg';
-import { ReactComponent as LogoWhiteText } from '../../assets/images/logos/white-logo-text.svg';
+import { ReactComponent as LogoDarkIcon } from '../assets/images/logos/dark-logo-icon.svg';
+import { ReactComponent as LogoDarkText } from '../assets/images/logos/dark-logo-text.svg';
+import { ReactComponent as LogoWhiteIcon } from '../assets/images/logos/white-logo-icon.svg';
+import { ReactComponent as LogoWhiteText } from '../assets/images/logos/white-logo-text.svg';
 
 const AuthLogo = () => {
   const isDarkMode = useSelector((state) => state.customizer.isDark);
