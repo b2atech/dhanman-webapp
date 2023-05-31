cebfea10a3140dab14d28548c62ef9a0b40b17bf
ybp345 <ybp345@gmail.com>
2023-05-31 18:12:04 +0530

    create screen for coa

--- src/views/masters/coa.js
+++ src/views/masters/coa.js
@@ -1,90 +1,38 @@
-import React from 'react';
-import { Table, Button } from 'reactstrap';
-// import BreadCrumbs from '../../layouts/breadcrumbs/BreadCrumbs';
-import ComponentCard from '../../components/ComponentCard';
-
+import React, { useState } from 'react';
+import BreadCrumbs from '../../layouts/breadcrumbs/BreadCrumbs';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 const COA = () => {
+  const [rowData] = useState([
+    { accountcode: "001", accountname: "Cash", accountcategory: 1, parentid: 2, activestatus: "Yes" },
+    { accountcode: "002", accountname: "liabilities", accountcategory: 2, parentid: 1, activestatus: "Yes" },
+    { accountcode: "003", accountname: "equity", accountcategory: 3, parentid: 5, activestatus: "Yes" },
+    { accountcode: "004", accountname: "expenses", accountcategory: 4, parentid: 3, activestatus: "Yes" },
+    { accountcode: "005", accountname: "revenue", accountcategory: 5, parentid: 4, activestatus: "Yes" }
+  ]);
+
+  const [columnDefs] = useState([
+    { checkboxSelection: true, width: 50 },
+    { field: 'accountcode', headerName: 'Account Code' },
+    { field: 'accountname', headerName: 'Account Name' },
+    { field: 'accountcategory', headerName: 'Account Category' },
+    { field: 'parentid', headerName: 'Parent Id' },
+    { field: 'activestatus', headerName: 'Active Status' }
+  ]);
 
   return (
     <>
-      {/* class="btn text-right"  className="mr-auto" */}
-      <div className="button-group" style={{ float: 'right' }}>
-        <Button className="btn" color="primary">
-          Save
-        </Button>
-        <Button className="btn" color="success">
-          Add Payment
-        </Button>
-        <Button className="btn" color="primary">
-          Finalize for sending
-        </Button>
+    <BreadCrumbs />
+      <div className="ag-theme-alpine" style={{ height: 400, width: 1000 }}>
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={columnDefs}
+        ></AgGridReact>
       </div>
-      <br />
-      <br />
-      <ComponentCard
-        title="Chart Of Accounts"
-        subtitle={
-          <p>
-            {/* Use <code>striped</code> to add zebra-striping to any table row within the{' '}
-            <code>&lt;tbody&gt;</code>. */}
-            A chart of accounts (COA) is a categorized list of all the financial accounts used by a business or organization to record and track its financial transactions.
-          </p>
-        }
-      >
-        <Table striped responsive>
-          <thead>
-            <tr>
-              <th scope="col">Accoun Code</th>
-              <th scope="col">Account Name</th>
-              <th scope="col">Account Category</th>
-              <th scope="col">Parent Id</th>
-              <th scope="col">Active Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">100</th>
-              <td> Cash</td>
-              <td>Assets</td>
-              <td>1</td>
-              <td>Yes</td>
-            </tr>
-            <tr>
-              <th scope="row">200</th>
-              <td>Loans Payable</td>
-              <td>Liabilities</td>
-              <td>2</td>
-              <td>No</td>
-            </tr>
-            <tr>
-              <th scope="row">300</th>
-              <td>Owners Capital</td>
-              <td>Equity</td>
-              <td>3</td>
-              <td>Yes</td>
-            </tr>
-            <tr>
-              <th scope="row">400</th>
-              <td>Sales Revenue</td>
-              <td>Revenue</td>
-              <td>4</td>
-              <td>No</td>
-            </tr>
-            <tr>
-              <th scope="row">500</th>
-              <td>Rent Expense</td>
-              <td>Expenses</td>
-              <td>5</td>
-              <td>Yes</td>
-            </tr>
-          </tbody>
-        </Table>
-      </ComponentCard>
-      {/*--------------------------------------------------------------------------------*/}
-      {/*End Inner Div*/}
-      {/*--------------------------------------------------------------------------------*/}
     </>
   );
 };
-export default COA
+
+export default COA;
