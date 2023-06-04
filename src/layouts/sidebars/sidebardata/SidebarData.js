2846a803c9f6eaa97d9b3aaf9a2b101ddfada254
appseedspranali <appseeds.pranali.@gmail.com>
2023-06-04 11:25:04 +0530

    List of UI for Vendors Tag Types

--- src/layouts/sidebars/sidebardata/SidebarData.js
+++ src/layouts/sidebars/sidebardata/SidebarData.js
@@ -17,6 +17,27 @@ const SidebarData = [
         collapisble: false,
       },
       {
+        title: 'Vendors',
+        href: '/masters/Vendors',
+        icon: <MaterialIcon icon="radio_button_checked" />,
+        id: 1.1,
+        collapisble: false,
+      },
+      {
+        title: 'Tags',
+        href: '/masters/Tags',
+        icon: <MaterialIcon icon="radio_button_checked" />,
+        id: 1.1,
+        collapisble: false,
+      },
+      {
+        title: 'TagTypes',
+        href: '/masters/TagTypes',
+        icon: <MaterialIcon icon="radio_button_checked" />,
+        id: 1.1,
+        collapisble: false,
+      },
+      {
         title: 'Invoice Status',
         href: '/masters/Invoice',
         icon: <MaterialIcon icon="radio_button_checked" />,
