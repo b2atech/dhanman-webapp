28d0d66f74641932ae501711b15cf2986fc18cec
bharat.mane <bharat.mane@gmail.com>
2023-07-24 13:10:34 +0530

    chors: fix list errors

--- src/api/services/BillService.tsx
+++ src/api/services/BillService.tsx
@@ -4,7 +4,7 @@ import { defineCancelApiObject } from '../axiosUtils';
 export const BillAPI = {
   get: async function (clientId: string, cancel = false) {
     const response = await api.request({
-      url: `/Vendor/${clientId}`,
+      url: `/Vendor/GetAllVendors/${clientId}`,
       method: 'GET',
       signal: cancel ? cancelApiObject[this.get.name].handleRequestCancellation().signal : undefined
     });
