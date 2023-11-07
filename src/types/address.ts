5cb6eafc92059248642b1b4dc01b2afaa545eeca
ybp345 <ybp345@gmail.com>
2023-11-07 11:25:07 +0530

    add country state city proptes in add vendor popup

---
+++ src/types/address.ts
@@ -0,0 +1,18 @@
+export interface ICountry {
+  id: string;
+  name: string;
+  isoAlphaCode: string;
+}
+
+export interface IState {
+  id: string;
+  name: string;
+  countryId: string;
+}
+
+export interface ICity {
+  id: string;
+  name: string;
+  postalCode: string;
+  stateId: string;
+}
