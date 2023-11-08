978bd566af0f28c9cbb0c481572a70d935adeb17
karankhamkar <93980611+karankhamkar@users.noreply.github.com>
2023-11-08 10:57:44 +0530

    chore: refactor code

--- src/pages/sales/createinvoice/Customer/CustomerDetails.tsx
+++ src/pages/sales/createinvoice/Customer/CustomerDetails.tsx
@@ -126,7 +126,7 @@ const CustomerView = ({ data }: any) => {
                       <Grid item xs={12} md={6}>
                         <Stack spacing={0.5}>
                           <Typography color="secondary">Address</Typography>
-                          <Typography>{data.addressLine}</Typography>
+                          <Typography>{address?.city}</Typography>
                         </Stack>
                       </Grid>
                     </ListItem>
