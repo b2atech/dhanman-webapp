09d9df0e66b67d693ecf467b2296698df3b3691d
Shubhdongare <shubhamdongare2511@gmail.com>
2023-12-23 16:44:15 +0530

    feat: view bill foe bills in bill list

--- src/pages/purchase/bills/details.tsx
+++ src/pages/purchase/bills/details.tsx
@@ -7,7 +7,6 @@ import {
   Box,
   Button,
   Divider,
-  FormControl,
   Grid,
   Stack,
   Table,
@@ -28,8 +27,10 @@ import {
 import Loader from 'components/Loader';
 import MainCard from 'components/MainCard';
 import { getBillById } from 'api/services/BillService';
+import { getCompanyDetail } from 'api/services/CommonService';
+import config from 'config';
 
-//import { useSelector } from 'store';
+// import { useSelector } from 'store';
 
 // types
 import { IBillType } from 'types/bill';
@@ -45,14 +46,23 @@ import ReactToPrint from 'react-to-print';
 
 const Details = () => {
   const theme = useTheme();
-  const navigation = useNavigate();
   const { id } = useParams();
-  const [list, setList] = useState<IBillType>();
+  const navigation = useNavigate();
+  const [company, setCompany] = useState<any>();
 
-  const [loading, setLoading] = useState<boolean>(true);
-  //const { list1 } = useSelector((state) => state.invoice);
+  // const { list } = useSelector((state) => state.bill);
+  const [Bill, setList] = useState<IBillType>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const companyId: string = String(config.companyId);
 
   const componentRef: React.Ref<HTMLDivElement> = useRef(null);
+  useEffect(() => {
+    if (companyId) {
+      getCompanyDetail(companyId).then((companyInfo) => {
+        setCompany(companyInfo);
+      });
+    }
+  }, [companyId]);
 
   useEffect(() => {
     if (id) {
@@ -63,25 +73,28 @@ const Details = () => {
     }
   }, [id]);
 
-  const today = new Date(`${list?.billDate}`).toLocaleDateString('en-GB', {
+  const today = new Date(`${Bill?.billDate}`).toLocaleDateString('en-GB', {
     month: 'numeric',
     day: 'numeric',
     year: 'numeric'
   });
 
-  const due_dates = new Date(`${list?.dueDate}`).toLocaleDateString('en-GB', {
+  const due_dates = new Date(`${Bill?.dueDate}`).toLocaleDateString('en-GB', {
     month: 'numeric',
     day: 'numeric',
     year: 'numeric'
   });
 
-  const subTotal = (list?.lines ?? []).reduce((total, row) => {
+  const subTotal = (Bill?.lines ?? []).reduce((total, row) => {
     return total + row.amount;
   }, 0);
   if (loading) return <Loader />;
 
-  const taxRate = (Number(list?.tax) * subTotal) / 100;
-  const discountRate = (Number(list?.discount) * subTotal) / 100;
+  const taxRate = (Number(Bill?.tax) * subTotal) / 100;
+  const discountRate = (Number(Bill?.discount) * subTotal) / 100;
+  const discountStyle = {
+    color: '#3EB489'
+  };
 
   return (
     <MainCard content={false}>
@@ -92,7 +105,7 @@ const Details = () => {
               <IconButton onClick={() => navigation(`/purchase/bills/edit/${id}`)}>
                 <EditOutlined style={{ color: theme.palette.grey[900] }} />
               </IconButton>
-              <PDFDownloadLink document={<ExportPDFView list={list} />} fileName={`${list?.billNumber}-${list?.vendor?.firstName}.pdf`}>
+              <PDFDownloadLink document={<ExportPDFView list={Bill} />} fileName={`${Bill?.billNumber}-${Bill?.vendor?.firstName}.pdf`}>
                 <IconButton>
                   <DownloadOutlined style={{ color: theme.palette.grey[900] }} />
                 </IconButton>
@@ -113,22 +126,23 @@ const Details = () => {
         </Box>
         <Box sx={{ p: 2.5 }} id="print" ref={componentRef}>
           <Grid container spacing={2.5}>
-            <Grid item xs={12}>
+            <Grid item xs={12} sx={{ marginTop: '-30px' }}>
               <Stack direction={{ xs: 'column', sm: 'row' }} justifyContent="space-between">
                 <Box>
                   <Stack direction="row" spacing={2}>
                     <LogoSection />
-                    <Chip
-                      label={list?.billStatus}
-                      variant="light"
-                      size="small"
-                      color={list?.billStatus === 'Closed' ? 'error' : list?.billStatus === 'Paid' ? 'success' : 'info'}
-                    />
                   </Stack>
+                  {/* <Typography color="secondary">{list?.bill_id}</Typography> */}
                 </Box>
                 <Box>
                   <Stack direction="row" spacing={1} justifyContent="flex-end">
-                    <Typography variant="subtitle1">Date</Typography>
+                    <Chip
+                      label={Bill?.billStatus}
+                      variant="light"
+                      size="small"
+                      color={Bill?.billStatus === 'Closed' ? 'error' : Bill?.billStatus === 'Paid' ? 'success' : 'info'}
+                    />
+                    <Typography variant="subtitle1">Bill Date</Typography>
                     <Typography color="secondary">{today}</Typography>
                   </Stack>
                   <Stack direction="row" spacing={1} justifyContent="flex-end">
@@ -140,29 +154,49 @@ const Details = () => {
                 </Box>
               </Stack>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <MainCard>
-                <Stack spacing={1}>
-                  <Typography variant="h5">From:</Typography>
-                  <FormControl sx={{ width: '100%' }}>
-                    <Typography color="secondary">Belle J. Richter</Typography>
-                    <Typography color="secondary">1300 Cooks Mine, NM 87829</Typography>
-                    <Typography color="secondary">305-829-7809</Typography>
-                    <Typography color="secondary">belljrc23@gmail.com</Typography>
-                  </FormControl>
-                </Stack>
+            <Grid item xs={12} sm={6} sx={{ marginTop: '-10px' }}>
+              <MainCard sx={{ minHeight: 130 }}>
+                <Grid container>
+                  <Grid item xs={12} sm={8} md={5}>
+                    <Stack spacing={2}>
+                      <Typography variant="h5">
+                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                          <span style={{ paddingRight: '10px' }}>From:</span>
+                          <span style={{ whiteSpace: 'nowrap' }}>{company?.name || ''}</span>
+                        </Box>
+                        <Stack sx={{ width: '100%' }}>
+                          <Typography color="secondary">{company?.email || ''}</Typography>
+                          <Typography color="secondary">{`${company?.addressLine || ''} ,\u00A0\u00A0 ${
+                            company?.phoneNumber || ''
+                          }`}</Typography>
+                          <Typography color="secondary">GSTIN: {company?.gstIn || ''}</Typography>
+                        </Stack>
+                      </Typography>
+                      {loading ? <Loader /> : ''}
+                    </Stack>
+                  </Grid>
+                </Grid>
               </MainCard>
             </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12} sm={6} sx={{ marginTop: '-10px' }}>
               <MainCard>
-                <Stack spacing={1}>
-                  <Typography variant="h5">To:</Typography>
-                  <FormControl sx={{ width: '100%' }}>
-                    <Typography color="secondary">{`${list?.vendor.firstName} ${list?.vendor.lastName}`}</Typography>
-                    <Typography color="secondary">{list?.vendor.addressLine}</Typography>
-                    <Typography color="secondary">{list?.vendor.phoneNumber}</Typography>
-                    <Typography color="secondary">{list?.vendor.email}</Typography>
-                  </FormControl>
+                <Stack spacing={2}>
+                  <Typography variant="h5">
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                      <span style={{ paddingRight: '10px' }}>To:</span>
+                      <span>{`${Bill?.vendor?.firstName} ${Bill?.vendor?.lastName}`}</span>
+                    </Box>
+                    <Stack sx={{ width: '100%' }}>
+                      <Typography variant="subtitle1"></Typography>
+                      <Typography color="secondary">
+                        {Bill?.vendor?.addressLine && Bill?.vendor?.phoneNumber
+                          ? `${Bill.vendor.addressLine}, \u00A0\u00A0${Bill.vendor.phoneNumber}`
+                          : `${Bill?.vendor?.addressLine || ''} ${Bill?.vendor?.phoneNumber || ''}`}
+                      </Typography>
+                      <Typography color="secondary">{Bill?.vendor?.email}</Typography>
+                      {Bill?.vendor?.gstIn && <Typography color="secondary">GSTIN: {Bill.vendor.gstIn}</Typography>}
+                    </Stack>
+                  </Typography>
                 </Stack>
               </MainCard>
             </Grid>
@@ -172,22 +206,38 @@ const Details = () => {
                   <TableHead>
                     <TableRow>
                       <TableCell>#</TableCell>
+                      <TableCell>PO No</TableCell>
+                      <TableCell>PO Date</TableCell>
                       <TableCell>Name</TableCell>
                       <TableCell>Description</TableCell>
                       <TableCell align="right">Quantity</TableCell>
                       <TableCell align="right">Price</TableCell>
-                      <TableCell align="right">Amount</TableCell>
+                      <TableCell align="right">Fees</TableCell>
+                      <TableCell align="right">Discount</TableCell>
+                      <TableCell align="right">Taxable Amount</TableCell>
+                      <TableCell align="right">CGST Amount</TableCell>
+                      <TableCell align="right">SGST Amount</TableCell>
+                      <TableCell align="right">IGST Amount</TableCell>
+                      <TableCell align="right">Total Amount</TableCell>
                     </TableRow>
                   </TableHead>
                   <TableBody>
-                    {list?.lines?.map((row: any, index) => (
+                    {Bill?.lines?.map((row: any, index) => (
                       <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                         <TableCell>{index + 1}</TableCell>
+                        <TableCell>{}</TableCell>
+                        <TableCell>{}</TableCell>
                         <TableCell>{row.name}</TableCell>
                         <TableCell>{row.description}</TableCell>
                         <TableCell align="right">{row.quantity}</TableCell>
                         <TableCell align="right">{row.price}</TableCell>
-                        <TableCell align="right">{row.amount}</TableCell>
+                        <TableCell align="right">{row.fees}</TableCell>
+                        <TableCell align="right">{row.discount}</TableCell>
+                        <TableCell align="right">{row.taxableAmount}</TableCell>
+                        <TableCell align="right">{row.cgstTaxAmount}</TableCell>
+                        <TableCell align="right">{row.sgstTaxAmount}</TableCell>
+                        <TableCell align="right">{row.igstTaxAmount}</TableCell>
+                        <TableCell align="right">₹ {Number(row.price * row.quantity).toFixed(2)}</TableCell>
                       </TableRow>
                     ))}
                   </TableBody>
@@ -198,15 +248,21 @@ const Details = () => {
               <Divider sx={{ borderWidth: 1 }} />
             </Grid>
             <Grid item xs={12} sm={6} md={8}></Grid>
+            <Grid item xs={12} sm={6} md={8}>
+              <Stack direction="row" spacing={1}>
+                <Typography color="secondary">Note: </Typography>
+                <Typography>{Bill?.note}</Typography>
+              </Stack>
+            </Grid>
             <Grid item xs={12} sm={6} md={4}>
-              <Stack spacing={2}>
+              <Stack spacing={1} sx={{ paddingRight: '22px', marginTop: '-20px' }}>
                 <Stack direction="row" justifyContent="space-between">
                   <Typography color={theme.palette.grey[500]}>Sub Total:</Typography>
                   <Typography variant="subtitle1">₹ {subTotal}</Typography>
                 </Stack>
                 <Stack direction="row" justifyContent="space-between">
                   <Typography color={theme.palette.grey[500]}>Discount:</Typography>
-                  <Typography>₹ {discountRate}</Typography>
+                  <Typography style={discountStyle}>₹ {discountRate}</Typography>
                 </Stack>
                 <Stack direction="row" justifyContent="space-between">
                   <Typography color={theme.palette.grey[500]}>Tax:</Typography>
@@ -214,20 +270,14 @@ const Details = () => {
                 </Stack>
                 <Stack direction="row" justifyContent="space-between">
                   <Typography variant="subtitle1">Grand Total:</Typography>
-                  <Typography variant="subtitle1">₹ {list?.totalAmount.toFixed(2)}</Typography>
+                  <Typography variant="subtitle1">₹ {Bill?.totalAmount.toFixed(2)}</Typography>
                 </Stack>
-              </Stack>
-            </Grid>
-            <Grid item xs={12}>
-              <Stack direction="row" spacing={1}>
-                <Typography color="secondary">Note: </Typography>
-                <Typography>{list?.note}</Typography>
               </Stack>
             </Grid>
           </Grid>
         </Box>
         <Stack direction="row" justifyContent="flex-end" spacing={2} sx={{ p: 2.5, a: { textDecoration: 'none', color: 'inherit' } }}>
-          <PDFDownloadLink document={<ExportPDFView list={list} />} fileName={`${list?.billNumber}-${list?.vendor.firstName}.pdf`}>
+          <PDFDownloadLink document={<ExportPDFView list={Bill} />} fileName={`${Bill?.billNumber}-${Bill?.vendor.firstName}.pdf`}>
             <Button variant="contained" color="primary">
               Download
             </Button>
