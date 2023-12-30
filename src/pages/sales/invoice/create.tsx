5896e93bd93c2f209378eab3dc9d808a2c346f35
karankhamkar <93980611+karankhamkar@users.noreply.github.com>
2023-12-30 15:55:52 +0530

    chore: spell

--- src/pages/sales/invoice/create.tsx
+++ src/pages/sales/invoice/create.tsx
@@ -257,7 +257,7 @@ const Createinvoice = () => {
     } else {
       console.log('No customer selected');
       setIsCustomerSelected(false);
-      return <Typography color="red">* Please select Customer </Typography>;
+      return <Typography color="red">* Please select customer </Typography>;
     }
   };
 
